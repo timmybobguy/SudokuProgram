c1bf6222b4b13a7121e4c5baae3fc21d08c02b47
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-08-19 19:08:03 +1200

    Fixed and working with checking :)

--- TestSudoku/Program.cs
+++ TestSudoku/Program.cs
@@ -8,13 +8,15 @@ namespace TestSudoku
 {
     class Program
     {
-        static int squareHeight = 3;
+        static int squareHeight = 2;
         static int squareWidth = 2;
-        static int gridWidth = squareWidth * squareWidth;
-        static int gridHeight = squareHeight * squareHeight;
-        static int[] numbersArray = { 2, 1, 5, 4, 3, 6, 3, 4, 2, 6, 5, 1, 5, 6, 1, 3, 2, 4, 6, 3, 4, 5, 1, 2, 4, 2, 3, 1, 6, 5, 1, 5, 6, 2, 4, 3 };
+        static int gridWidth = squareWidth * squareHeight;
+        static int gridHeight = squareHeight * squareWidth;
+        //static int[] numbersArray = { 6,5,1,3,4,2,4,3,2,1,6,5,2,4,6,5,1,3,5,1,3,4,2,6,1,2,5,6,3,4,3,6,4,2,5,1 }; // 3x2
+        static int[] numbersArray = { 1,2,3,4,3,4,1,2,2,3,4,1,4,1,2,3 }; // 2x2
         static void Main(string[] args)
         {
+            /*
             int row = 0;
             int column = 8;
             int squareIndex = 2;
@@ -23,7 +25,8 @@ namespace TestSudoku
             Console.WriteLine(GetByColumn(column, row));
             Console.WriteLine(GetByRow(row, column));
             Console.WriteLine(GetBySquare(squareIndex, squareOffset));
-
+            */
+            /*
             Console.WriteLine(RowValid(0));
             Console.WriteLine(RowValid(1));
             Console.WriteLine(RowValid(2));
@@ -33,15 +36,22 @@ namespace TestSudoku
             Console.WriteLine(ColumnValid(1));
             Console.WriteLine(ColumnValid(2));
             Console.WriteLine(ColumnValid(3));
-
+            */
+            /*
             Console.WriteLine(SquareValid(0));
             Console.WriteLine(SquareValid(1));
             Console.WriteLine(SquareValid(2));
             Console.WriteLine(SquareValid(3));
+            Console.WriteLine(SquareValid(4));
+            Console.WriteLine(SquareValid(5));
+            */
 
-            Console.WriteLine("puzzle is " + isPuzzleValid());
+            Console.WriteLine(DrawGrid());
+            Console.WriteLine("puzzle is " + IsPuzzleValid());
             Console.ReadKey();
+
         }
+
         static string DrawGrid()
         {
             string line = String.Empty;
@@ -67,20 +77,36 @@ namespace TestSudoku
             return index;
         }
 
-        static int GetBySquare(int squareIndex, int positionIndex)
+        static int GetBySquare(int squareIndex, int squareOffset)
         {
-            int index = 0;
+            int result = 0;
             for (int i = 1; i <= squareIndex; i++)
             {
-                bool isEndOfLine = i % squareWidth == 0 && positionIndex != 0;
-                index += (i % squareWidth == 0) ? ((squareWidth * squareWidth) * (squareHeight - 1)) + squareWidth : squareWidth;
+                if (i % (gridWidth / squareWidth) == 0)
+                {
+                    result += ((gridWidth * (squareHeight - 1)) + squareWidth);
+                }
+                else
+                {
+                    result += squareWidth;
+                }
             }
-            for (int i = 1; i <= positionIndex; i++)
+            int offsetResult = result;
+      
+            for (int i = 1; i <= squareOffset; i++)
             {
-                bool isEndOfLine = i % squareWidth == 0 && positionIndex != 0;
-                index += isEndOfLine ? (((squareWidth * squareWidth) - squareWidth) + 1) : 1;
+                if (i % (squareWidth) == 0)
+                {
+                   
+                    offsetResult += ((gridWidth - squareWidth) + 1);
+                }
+                else if (squareOffset != 0)
+                {
+                    
+                    offsetResult += 1;
+                }
             }
-            return index;
+            return offsetResult;
         }
 
         static void SetByColumn(int value, int columnIndex, int rowIndex)
@@ -147,7 +173,7 @@ namespace TestSudoku
         }
 
 
-        static bool isPuzzleValid()
+        static bool IsPuzzleValid()
         {
             for (int i = 0; i < gridHeight; i++)
             {
