a5ac4821935c5bf35c0f8f4fc40ba872af326876
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-08-19 19:12:26 +1200

    Added extra test

--- TestSudoku/Program.cs
+++ TestSudoku/Program.cs
@@ -8,12 +8,13 @@ namespace TestSudoku
 {
     class Program
     {
-        static int squareHeight = 2;
-        static int squareWidth = 2;
+        static int squareHeight = 3;
+        static int squareWidth = 3;
         static int gridWidth = squareWidth * squareHeight;
         static int gridHeight = squareHeight * squareWidth;
         //static int[] numbersArray = { 6,5,1,3,4,2,4,3,2,1,6,5,2,4,6,5,1,3,5,1,3,4,2,6,1,2,5,6,3,4,3,6,4,2,5,1 }; // 3x2
-        static int[] numbersArray = { 1,2,3,4,3,4,1,2,2,3,4,1,4,1,2,3 }; // 2x2
+        //static int[] numbersArray = { 1,2,3,4,3,4,1,2,2,3,4,1,4,1,2,3 }; // 2x2
+        static int[] numbersArray = { 7,3,5,6,1,4,8,9,2,8,4,2,9,7,3,5,6,1,9,6,1,2,8,5,3,7,4,2,8,6,3,4,9,1,5,7,4,1,3,8,5,7,9,2,6,5,7,9,1,2,6,4,3,8,1,5,7,4,9,2,6,8,3,6,9,4,7,3,8,2,1,5,3,2,8,5,6,1,7,4,9 };
         static void Main(string[] args)
         {
             /*
