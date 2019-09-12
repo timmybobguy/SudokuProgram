1091813e78b3f3844d2a46eafc9b51c38b5bde67
tib0259 <tib0259@L130-60822>
2019-09-13 10:06:03 +1200

    Url

--- SudokuUnitTests/UnitTest1.cs
+++ SudokuUnitTests/UnitTest1.cs
@@ -28,5 +28,28 @@ namespace SudokuUnitTests
 
             Assert.AreEqual(expected, theSudoku.numbersArray, "The array is reset to the original");
         }
+
+        [TestMethod]
+        public void TestRestart()
+        {
+            // Arrange 
+
+            Game theSudoku = new Game();
+
+            int[] expected = { 7, 3, 5, 6, 1, 4, 8, 9, 2, 8, 4, 2, 9, 7, 3, 5, 6, 1, 9, 6, 1, 2, 8, 5, 3, 7, 4, 2, 8, 6, 3, 4, 9, 1, 5, 7, 4, 1, 3, 8, 5, 7, 9, 2, 6, 5, 7, 9, 1, 2, 6, 4, 3, 8, 1, 5, 7, 4, 9, 2, 6, 8, 3, 6, 9, 4, 7, 3, 8, 2, 1, 5, 3, 2, 8, 5, 6, 1, 7, 4, 9 };
+            int[] test = { 2, 3, 5, 6, 3, 4, 8, 9, 2, 3, 4, 2, 2, 7, 3, 5, 6, 1, 9, 6, 1, 2, 8, 5, 3, 7, 4, 2, 8, 6, 3, 4, 9, 1, 5, 7, 4, 1, 3, 8, 5, 7, 9, 2, 6, 5, 7, 9, 1, 2, 6, 4, 3, 8, 1, 5, 7, 4, 9, 2, 6, 8, 3, 6, 9, 4, 7, 3, 8, 2, 1, 5, 3, 2, 8, 5, 6, 1, 7, 4, 9 };
+
+
+            // Act
+
+            theSudoku.numbersArray = test;
+            theSudoku.originalNumbersArray = expected;
+
+            theSudoku.Restart();
+
+            // Assert
+
+            Assert.AreEqual(expected, theSudoku.numbersArray, "The array is reset to the original");
+        }
     }
 }
