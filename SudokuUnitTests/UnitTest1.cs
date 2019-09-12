e0fdd37fd93934a0f5ac768b2aea3d3d8f3ffbf8
tib0259 <tib0259@L130-60822>
2019-09-13 11:17:46 +1200

    Added some tests

--- SudokuUnitTests/UnitTest1.cs
+++ SudokuUnitTests/UnitTest1.cs
@@ -30,26 +30,63 @@ namespace SudokuUnitTests
         }
 
         [TestMethod]
-        public void TestRestart()
+        public void TestGetMaxValue()
         {
             // Arrange 
 
             Game theSudoku = new Game();
 
-            int[] expected = { 7, 3, 5, 6, 1, 4, 8, 9, 2, 8, 4, 2, 9, 7, 3, 5, 6, 1, 9, 6, 1, 2, 8, 5, 3, 7, 4, 2, 8, 6, 3, 4, 9, 1, 5, 7, 4, 1, 3, 8, 5, 7, 9, 2, 6, 5, 7, 9, 1, 2, 6, 4, 3, 8, 1, 5, 7, 4, 9, 2, 6, 8, 3, 6, 9, 4, 7, 3, 8, 2, 1, 5, 3, 2, 8, 5, 6, 1, 7, 4, 9 };
-            int[] test = { 2, 3, 5, 6, 3, 4, 8, 9, 2, 3, 4, 2, 2, 7, 3, 5, 6, 1, 9, 6, 1, 2, 8, 5, 3, 7, 4, 2, 8, 6, 3, 4, 9, 1, 5, 7, 4, 1, 3, 8, 5, 7, 9, 2, 6, 5, 7, 9, 1, 2, 6, 4, 3, 8, 1, 5, 7, 4, 9, 2, 6, 8, 3, 6, 9, 4, 7, 3, 8, 2, 1, 5, 3, 2, 8, 5, 6, 1, 7, 4, 9 };
-
+            int expected = 5;
 
             // Act
 
-            theSudoku.numbersArray = test;
-            theSudoku.originalNumbersArray = expected;
-
-            theSudoku.Restart();
+            theSudoku.SetMaxValue(5);
 
             // Assert
 
-            Assert.AreEqual(expected, theSudoku.numbersArray, "The array is reset to the original");
+            Assert.AreEqual(expected, theSudoku.GetMaxValue(), "The max value is set properly");
+        }
+
+        [TestMethod]
+        public void TestSetCellMethod()
+        {
+            // Arrange 
+
+            Game theSudoku = new Game();
+
+            int[] numbers = { 7, 3, 5, 6, 1, 4, 8, 9 };
+            theSudoku.Set(numbers);
+
+            int expected = 3;
+
+            // Act
+
+            theSudoku.SetCell(3, 0);
+
+            // Assert
+
+            Assert.AreEqual(expected, theSudoku.GetCell(0), "The square width is set properly");
+        }
+
+        [TestMethod]
+        public void TestSetCellMethod2()
+        {
+            // Arrange 
+
+            Game theSudoku = new Game();
+
+            int[] numbers = { 7, 3, 5, 6, 1, 4, 8, 9 };
+            theSudoku.Set(numbers);
+
+            int expected = 5;
+
+            // Act
+
+            theSudoku.SetCell(5, 7);
+
+            // Assert
+
+            Assert.AreEqual(expected, theSudoku.GetCell(7), "The square width is set properly");
         }
     }
 }
